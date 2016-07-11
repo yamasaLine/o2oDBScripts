96e9dbf6026e3d03e41fd644c8f28c9c8d3acc7a
yamasaLine <yamasa@live.com>
2016-07-11 11:29:47 +0800

    Update fake_data_insert_script_base.sql

--- fake_data_insert_script_base.sql
+++ fake_data_insert_script_base.sql
@@ -13,16 +13,16 @@ begin
   insert into open_account_by_manager_per_week
   select '460000000098', 2016, @index  ,  dbo.randbetween(77,120), 0
 
-  insert into open_account_by_manager_per_day 
+  insert into open_account_by_manager_per_week
   select '050000000098', 2016, @index,  dbo.randbetween(77,120, 0
 
-  insert into open_account_by_manager_per_day 
+  insert into open_account_by_manager_per_week
   select '731000000039', 2016, @index,  dbo.randbetween(77,120), 0
 
-  insert into open_account_by_manager_per_day 
+  insert into open_account_by_manager_per_week
   select '731000000036', 2016, @index,  dbo.randbetween(77,120), 0
 
-  insert into open_account_by_manager_per_day 
+  insert into open_account_by_manager_per_week
   select '731000000033', 2016, @index,  dbo.randbetween(77,120), 0
   set @index = @index +1 
   end
