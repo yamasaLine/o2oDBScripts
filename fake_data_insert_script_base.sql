d03b3fd82d6e75d564409f7190efdf6b4d98401a
yamasaLine <yamasa@live.com>
2016-07-11 11:28:28 +0800

    Update fake_data_insert_script_base.sql

--- fake_data_insert_script_base.sql
+++ fake_data_insert_script_base.sql
@@ -10,12 +10,20 @@ set @index = 1
 
 while @index <= @total_rows 
 begin
+  insert into open_account_by_manager_per_week
+  select '460000000098', 2016, @index  ,  dbo.randbetween(77,120), 0
+
   insert into open_account_by_manager_per_day 
-  select '460000000098', 2016,  datepart(dy, '2016-06-' + (cast(@index as varchar))),  dbo.randbetween(10,30), 0
+  select '050000000098', 2016, @index,  dbo.randbetween(77,120, 0
+
   insert into open_account_by_manager_per_day 
-  select '050000000098', 2016, datepart(dy, '2016-06-' + (cast(@index as varchar))),  dbo.randbetween(10,30), 0
+  select '731000000039', 2016, @index,  dbo.randbetween(77,120), 0
+
   insert into open_account_by_manager_per_day 
-  select '731000000039', 2016, datepart(dy, '2016-06-' + (cast(@index as varchar))),  dbo.randbetween(10,30), 0
+  select '731000000036', 2016, @index,  dbo.randbetween(77,120), 0
+
+  insert into open_account_by_manager_per_day 
+  select '731000000033', 2016, @index,  dbo.randbetween(77,120), 0
   set @index = @index +1 
   end
 
