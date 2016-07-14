a6450dcaa0f2372a988fbad68d0b1f5cfb4e4afe
yamasaLine <yamasa@live.com>
2016-07-14 16:48:20 +0800

    add trigger which auto-updates update_time

--- open_account_by_manager_per_day indexes.sql
+++ open_account_by_manager_per_day indexes.sql
@@ -1,6 +1,7 @@
 use datasearch
 
 CREATE TABLE dbo.open_account_by_manager_per_month_with_chanel (
+  id BIGINT IDENTITY(1, 1) PRIMARY KEY,
   manager_code VARCHAR(15) NOT NULL DEFAULT '',
   archive_year INT NOT NULL DEFAULT 0,
   archive_month_of_year INT NOT NULL DEFAULT 0,
@@ -8,7 +9,9 @@ CREATE TABLE dbo.open_account_by_manager_per_month_with_chanel (
 	channel_type INT NOT NULL DEFAULT 0,
   open_account_count INT NOT NULL DEFAULT 0,
   rank_in_department INT NOT NULL DEFAULT 0,
-	percentage DECIMAL(5, 2) NULL
+	percentage DECIMAL(5, 2) NULL,
+  create_time DATETIME NOT NULL DEFAULT GETDATE(),
+  update_time DATETIME NOT NULL DEFAULT GETDATE()
 )
 
 alter table dbo.open_account_by_manager_per_month_with_chanel
@@ -19,4 +22,24 @@ with (PAD_INDEX = ON, IGNORE_DUP_KEY = OFF , FILLFACTOR = 80)
 create NONCLUSTERED index idx_archive_year_day_source
 ON dbo.open_account_by_manager_per_month_with_chanel (archive_year, archive_month_of_year, source_type)
 with (PAD_INDEX = ON, ONLINE = ON, FILLFACTOR = 80)
+go
+
+create trigger oabmpmwc_trg_updated_time on dbo.open_account_by_manager_per_month_with_chanel
+for update
+as
+begin
+
+    -- nothing to do?
+    if (@@rowcount = 0)
+      return;
+
+    update d
+    set
+       update_time = getdate()
+    from
+       dbo.open_account_by_manager_per_month_with_chanel d join inserted i
+    on d.id = i.id;
+
+end
+go
 
